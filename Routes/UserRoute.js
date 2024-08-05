110578b6c10a0efe33f2a50ff5441fb0fe059ed9
jdaniel231 <jdaniel231@gmail.com>
2024-08-05 01:21:20 -0300

    api category

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -22,7 +22,33 @@ router.post('/adminlogin', (req, res) => {
       res.cookie('token', token);
       return res.json({ loginStatus: true });
     } else {
-      return res.json({ loginStatus: false, message: "Invalid Credentials" });
+      return res.json({ loginStatus: false, message: "wrong email or password" });
+    }
+  });
+});
+
+router.get('/category', (req, res) => {
+  const sql = "SELECT * FROM categories";
+  pool.query(sql, (err, result) => {
+    if (err) {
+      console.error("Error getting categories:", err);
+      return res.status(500).json({ Status: false, Message: err.message });
+    }
+    return res.json({ Status: true, Result: result.rows });
+  });
+});
+
+router.post('/add_category', (req, res) => {
+  const sql = "INSERT INTO categories (name) VALUES ($1) RETURNING id";
+  pool.query(sql, [req.body.name], (err, result) => {
+    if (err) {
+      console.error("Error inserting category:", err);
+      return res.status(500).json({ Status: false, message: err.message });
+    }
+    if (result.rows.length > 0) {
+      return res.json({ Status: true, message: 'Categoria adicionada com sucesso', id: result.rows[0].id });
+    } else {
+      return res.status(500).json({ Status: false, message: "Erro ao inserir a categoria." });
     }
   });
 });
